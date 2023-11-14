2592f389cec8e2bac33039c37c0c95dc524282d2
BuildTools <unconfigured@null.spigotmc.org>
2023-11-15 00:49:25 +0700

    config CG (baseline)

--- CG.m
+++ CG.m
@@ -1,5 +1,16 @@
-function [xmin,fmin,IFLAG,nReset] = CG(x0,epsilon,mu,eta,itmax,option)
+function [xmin,fmin,Xk,Fk,Gk,Lk,nF,nG,IFLAG,nReset] = CG(x0,epsilon,mu,eta,itmax,option)
 g = zeros(2,itmax);
 s = zeros(2,itmax);
 nReset = zeros(itmax);
+xmin = x0;
+fmin = zeros(itmax);
+Xk = zeros(2,itmax);
+Fk = zeros(itmax);
+Lk = zeros(itmax);
+nF = 0;
+nG = 0;
+IFLAG = -99;
+[fmin(k), g(:,k)] = Rosenbrock(x0);
+
+
 end
