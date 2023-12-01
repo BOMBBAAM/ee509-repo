82b7841e97a2cc7409b9ba986e33db9657a01b7b
BuildTools <unconfigured@null.spigotmc.org>
2023-12-01 09:11:13 +0700

    initialize golden function

---
+++ golden.m
@@ -0,0 +1,8 @@
+%% Golden Section
+function [x_min,f_min,IFLAG,IFunc] = golden(x_0,epsilon,itmax)
+    x_min = zeros(itmax,1);
+    f_min = zeros(itmax,1);
+    % IFLAG 0 if success, -999 otherwise
+    IFLAG = -999; % initial condition
+    IFunc
+end
